f2a7e338efc9b58baef70c462e296b791ae72571
Naresh Sharma <sharmanaresh272@gmail.com>
2023-01-09 21:04:41 +0530

    .

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,13 @@
+public class ContainerWithMostWater {
+    public static int maxWater(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+        System.out.println(maxWater(height));
+    }
+}
