119a4ee6e85e2a652031655314d20286276d25e8
Naresh Sharma <sharmanaresh272@gmail.com>
2023-06-20 10:51:54 +0530

    .

--- StackDSAQuestions.java
+++ StackDSAQuestions.java
@@ -23,18 +23,51 @@ public class StackDSAQuestions {
         pushAtBottom(extractedData, stack);
     }
 
+    public static void stockSpan(int[] stocks, int[] span) {
+        Stack<Integer> stockStack = new Stack<>();
+
+        span[0] = 1;
+        stockStack.push(0);
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currentPrice = stocks[i];
+
+            while (!stockStack.isEmpty() && currentPrice > stocks[stockStack.peek()]) {
+                stockStack.pop();
+            }
+
+            if (stockStack.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                int previousHigh = stockStack.peek();
+                span[i] = i - previousHigh;
+            }
+
+            stockStack.push(i);
+        }
+    }
+
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
+        // Stack<Integer> stack = new Stack<>();
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
+        // stack.push(1);
+        // stack.push(2);
+        // stack.push(3);
 
+        // System.out.println(stack.toString());
 
-        System.out.println(stack.toString());
+        // // pushAtBottom(4, stack);
+        // reverseStack(stack);
 
-        pushAtBottom(4, stack);
+        // System.out.println(stack.toString());
 
-        System.out.println(stack.toString());
+        int[] stocks = { 100, 80, 60, 70, 60, 85, 100 };
+        int[] span = new int[stocks.length];
+
+        stockSpan(stocks, span);
+
+        for (int i : span) {
+            System.out.print(i + " ");
+        }
     }
 }
