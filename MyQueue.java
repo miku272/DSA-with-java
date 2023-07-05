1446e3dc38c73dc383cf57f72e0a8eacc6f9c829
Naresh Sharma <sharmanaresh272@gmail.com>
2023-07-05 14:17:53 +0530

    implemented queue using array

--- MyQueue.java
+++ MyQueue.java
@@ -1,17 +1,14 @@
 class ArrayQueue {
     int[] arr;
-    int size;
     int rear;
 
     ArrayQueue() {
         this.arr = new int[10];
-        this.size = 10;
         this.rear = -1;
     }
 
     ArrayQueue(int size) {
         this.arr = new int[size];
-        this.size = size;
         this.rear = -1;
     }
 
@@ -20,20 +17,58 @@ class ArrayQueue {
     }
 
     public void enqueue(int data) {
-        
+        if (rear == this.arr.length - 1) {
+            System.out.println("Queue is full");
+            return;
+        }
+
+        this.rear++;
+
+        this.arr[this.rear] = data;
     }
 
     public int dequeue() {
-        return 0;
+        if (this.isEmpty()) {
+            System.out.println("Queue is empty...");
+            return Integer.MIN_VALUE;
+        }
+
+        int front = this.arr[0];
+
+        for (int i = 0; i < this.rear; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+
+        rear--;
+
+        return front;
     }
 
     public int peek() {
-        return 0;
+        if (this.isEmpty()) {
+            System.out.println("Queue is empty...");
+            return Integer.MIN_VALUE;
+        }
+
+        return this.arr[0];
     }
 }
 
 public class MyQueue {
     public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue();
 
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
     }
 }
