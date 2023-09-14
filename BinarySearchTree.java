d6b2accd549644377f21d17330e4b50e93fd738c
Naresh Sharma <sharmanaresh272@gmail.com>
2023-09-14 20:19:50 +0530

    Some more programs in BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -67,6 +67,56 @@ public class BinarySearchTree {
         }
     }
 
+    public static Node findInorderSuccessor(Node root) {
+        while (root.getLeftNode() != null) {
+            root = root.getLeftNode();
+        }
+
+        return root;
+    }
+
+    public static Node deleteNodeInBST(Node root, int value) {
+        if (root.getData() < value) {
+            root.setRightNode(deleteNodeInBST(root.getRightNode(), value));
+        } else if (root.getData() > value) {
+            root.setLeftNode(deleteNodeInBST(root.getLeftNode(), value));
+        } else {
+            if (root.getLeftNode() == null && root.getRightNode() == null) {
+                return null;
+            }
+
+            if (root.getLeftNode() == null) {
+                return root.getRightNode();
+            } else if (root.getRightNode() == null) {
+                return root.getLeftNode();
+            }
+
+            Node inorderSuccessor = findInorderSuccessor(root.getRightNode());
+
+            root.setData(inorderSuccessor.getData());
+
+            root.setRightNode(deleteNodeInBST(root.getRightNode(), inorderSuccessor.getData()));
+        }
+
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.getData() >= k1 && root.getData() <= k2) {
+            printInRange(root.getLeftNode(), k1, k2);
+            System.out.print(root.getData() + " ");
+            printInRange(root.getRightNode(), k1, k2);
+        } else if (root.getData() < k1) {
+            printInRange(root.getLeftNode(), k1, k2);
+        } else {
+            printInRange(root.getRightNode(), k1, k2);
+        }
+    }
+
     public static void inorderTraversal(Node root) {
         if (root == null) {
             return;
@@ -78,7 +128,8 @@ public class BinarySearchTree {
     }
 
     public static void main(String[] args) {
-        int[] values = { 5, 1, 3, 4, 2, 7 };
+        // int[] values = { 5, 1, 3, 4, 2, 7 };
+        int[] values = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -87,6 +138,8 @@ public class BinarySearchTree {
 
         // inorderTraversal(root);
 
-        System.out.println(searchBST(root, 0));
+        // System.out.println(searchBST(root, 0));
+
+        printInRange(root, 5, 12);
     }
 }
